8c1289b4bfc234be831da7892bc85d9fe14c6d47
ThepsirinN <70904058+ThepsirinN@users.noreply.github.com>
2022-01-19 14:02:24 +0700

    Update Timer.py

--- Timer.py
+++ Timer.py
@@ -7,5 +7,6 @@ x = h*3600 + m*60 + s
 for i in range(x):
     sec = x - i
     print("(H:m:s) - ",sec//(60*60),":",sec//60 - ((60)*(sec//(60*60))),":",sec - 60*(sec//60))
+    # or print("(H:m:s) - ",sec//(60*60),":",sec//60 - ((60)*(sec//(60*60))),":",sec%60)
     time.sleep(1)
     clear_output()
